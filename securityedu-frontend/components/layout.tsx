b25ab0243d0d6244016031eb02b968fc305f9c57
Quan Do <minhquand3@gmail.com>
2022-10-23 18:54:30 +0000

    Add front page

---
+++ securityedu-frontend/components/layout.tsx
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+
+export const siteTitle = 'SecurityEdu';
+
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <h1 className={utilStyles.heading2Xl}>{siteTitle}</h1>
+          </>
+        ) : (
+          <>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{siteTitle}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
