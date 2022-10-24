cb86a9a94b4b1a15021bb38015ed1a5a77a95260
Quan Do <minhquand3@gmail.com>
2022-10-24 01:50:44 +0000

    Update categories pages

--- securityedu-frontend/pages/mobiles.tsx
+++ securityedu-frontend/pages/mobiles.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Layout from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
 const Mobiles: NextPage = () => {
   return (
     <Layout>
       <Head>
-        <title>{siteTitle}: Mobile Devices</title>
+        <title>Mobile Devices</title>
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Mobile Devices</h2>
